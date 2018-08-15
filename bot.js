0fb15d2065ddf63248b775b9b4d6affd02bfbd21
Stepasy <sad100500@gmail.com>
2018-08-15 11:55:15 +0300

    added scheduling

--- bot.js
+++ bot.js
@@ -1,19 +1,23 @@
 const TelegramBot = require('node-telegram-bot-api');
 const fs = require("fs");
-const token = '';
+const schedule = require('node-schedule');
+
+
+const token = '577254993:AAGkQzsSQj3ZMfnWJTIg6t2qgIYlHlKYJGg';
 const bot = new TelegramBot(token, { polling: true });
-const chatId = '@vagidashoroscopes';
+const chatId = '-1001290975094';
 
 let contents = fs.readFileSync("data.json");
 let jsonContent = JSON.parse(contents);
 let curDate = new Date().getHours() + ':' + new Date().getMinutes();
 
-setInterval(function() {
+let sendMessage = schedule.scheduleJob('48 11 * * *', function(){
     for (var i = 0; i < jsonContent.length; i++) {
-        var curDate = new Date().getHours() + ':' + new Date().getMinutes();
-        if (curDate == '1:05') {
-            bot.sendMessage(-1001290975094, jsonContent[i].sign)
-            bot.sendMessage(-1001290975094, jsonContent[i].content)
+
+            bot.sendMessage(chatId, jsonContent[i].sign + '\n' + '\n' + jsonContent[i].content)
+
         }
-    }
-}, 5000);
+  });
+
+
+  console.log('bot');
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const osmosis = require('osmosis');
 const fs = require('fs');
+const schedule = require('node-schedule');
 
 let signs = ['Aries', 'Lion', 'Sagittarius', 'Taurus', 'Virgo', 'Capricorn', 'Gemini', 'Libra', 'Aquarius', 'Cancer', 'Scorpio', 'Pisces'];
 let horoscope = [];
+
+let parseWebsite = schedule.scheduleJob('30 7 * * *', function(){
 
 for (let i = 0; i < signs.length; i++) {
 
@@ -15,13 +18,17 @@ for (let i = 0; i < signs.length; i++) {
         .find('.horoBlock > p[1]')
         .set('content')
         .data(function(data) {
-            console.log(data);
             horoscope.push(data);
         })
         .done(function() {
             fs.writeFile('data.json', JSON.stringify(horoscope, null, 4), function(err) {
                 if (err) console.error(err);
-                else console.log('Data Saved to data.json file');
             })
         });
 }
+});
+
+
+console.log('index');
+
+require('./bot');
