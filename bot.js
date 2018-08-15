02af27b54d791209a8a4a93318005e10293ee6e1
Stepasy <sad100500@gmail.com>
2018-08-15 15:54:01 +0300

    small refactor

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const TelegramBot = require('node-telegram-bot-api');
 const fs = require("fs");
 const schedule = require('node-schedule');
 
-
 const token = '577254993:AAGkQzsSQj3ZMfnWJTIg6t2qgIYlHlKYJGg';
 const bot = new TelegramBot(token, { polling: true });
 const chatId = '-1001290975094';
@@ -12,17 +11,14 @@ let jsonContent = JSON.parse(contents);
 let curDate = new Date().getHours() + ':' + new Date().getMinutes();
 
 bot.on('message', function (msg) {
-    var chatId = msg.chat.id;
+    let chatId = msg.chat.id;
     bot.sendMessage(chatId, 'rabotaem?');
 });
 
-let sendMessage = schedule.scheduleJob('48 10 * * *', function(){
-    for (var i = 0; i < jsonContent.length; i++) {
+let sendMessage = schedule.scheduleJob(process.env.SEND_TIME, function(){
+    for (let i = 0; i < jsonContent.length; i++) {
 
             bot.sendMessage(chatId, jsonContent[i].sign + '\n' + '\n' + jsonContent[i].content)
 
         }
   });
-
-
-  console.log('bot');
--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 require('./server');
+require('dotenv').config();
 const osmosis = require('osmosis');
 const fs = require('fs');
 const schedule = require('node-schedule');
 
-let signs = ['Aries', 'Lion', 'Sagittarius', 'Taurus', 'Virgo', 'Capricorn', 'Gemini', 'Libra', 'Aquarius', 'Cancer', 'Scorpio', 'Pisces'];
+const signs = ['Aries', 'Lion', 'Sagittarius', 'Taurus', 'Virgo', 'Capricorn', 'Gemini', 'Libra', 'Aquarius', 'Cancer', 'Scorpio', 'Pisces'];
+const urlP1 = 'http://orakul.com/horoscope/astrologic/general/';
+const urlP2 = '/today.html';
+
 let horoscope = [];
 
-let parseWebsite = schedule.scheduleJob('30 7 * * *', function(){
+let parseWebsite = schedule.scheduleJob(process.env.PARSE_TIME, function(){
 
-for (let i = 0; i < signs.length; i++) {
+    signs.forEach(element => {
+        let url = urlP1 + element + urlP2;
 
-    let url = 'http://orakul.com/horoscope/astrologic/general/' + signs[i] + '/today.html';
-
-    osmosis
-        .get(url)
-        .find('h1')
-        .set('sign')
-        .find('.horoBlock > p[1]')
-        .set('content')
-        .data(function(data) {
-            horoscope.push(data);
-        })
-        .done(function() {
-            fs.writeFile('data.json', JSON.stringify(horoscope, null, 4), function(err) {
-                if (err) console.error(err);
+        osmosis
+            .get(url)
+            .find('h1')
+            .set('sign')
+            .find('.horoBlock > p[1]')
+            .set('content')
+            .data(function(data) {
+                horoscope.push(data);
             })
-        });
-}
+            .done(function() {
+                fs.writeFile('data.json', JSON.stringify(horoscope, null, 4), function(err) {
+                    if (err) console.error(err);
+                })
+            });
+    });
 });
-
-
-console.log('index');
 
 require('./bot');
